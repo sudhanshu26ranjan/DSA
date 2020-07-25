651e557f047001020e5ca330cd133b995b8bc036
sudkumar <sudkumar@ea.com>
2020-07-25 16:33:38 +0530

    removed few unneccessary lines

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -7,17 +7,10 @@ public class Solution {
             prefix = prefix+(s.charAt(i));
             String rem = s.substring(0,i)+s.substring(i+1,s.length());
             printPermutation(prefix,rem);
-//            s = s.substring(0,i+1)+s.substring(i+1,s.length());
             prefix = prefix.substring(0,prefix.length()-1);
         }
     }
     public static void main(String[] args)throws InterruptedException{
-//        Thread.sleep(10000);
-//        System.out.println("Starting our application...");
-//        int i;
-//        for(i=-3; i!=0; i--);
-//        System.out.println(i);
-//        System.out.println("Terminating our application...");
         Solution obj = new Solution();
         obj.printPermutation("","abc");
     }
